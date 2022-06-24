afe94b6412742ae0d31910a33b73b9d2864356cf
Wouter Jehee <wouterjehee@tutanota.com>
2022-06-24 14:28:37 +0200

    Improved grass effect?

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+onready var sprite = $Sprite
+onready var effect = $Effect
+
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
-		var Effect = load("res://Effects/GrassEffect.tscn")
-		var e = Effect.instance()
-		var world = get_tree().current_scene
-		world.add_child(e)
-		e.global_position = global_position
-		
-		queue_free()
+		sprite.visible = false
+		effect.visible = true
+		effect.frame = 1
+		effect.play("Destroy")
+
+func _ready():
+	effect.visible = false
+
+func _on_Effect_animation_finished():
+	queue_free()
