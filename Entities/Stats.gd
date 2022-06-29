d5e7e0c442f3741a68604a57c1ba51673368e8ec
Wouter Jehee <wouterjehee@tutanota.com>
2022-06-29 17:55:41 +0200

    add hearts to ui, add tree and shadows

--- Entities/Stats.gd
+++ Entities/Stats.gd
@@ -1,11 +1,22 @@
 extends Node
 
-export var max_health = 1
-onready var health = max_health setget set_health
+export var max_health = 1 setget set_max_health
+var health = max_health setget set_health
 
 signal death
+signal health_changed(v)
+signal max_health_changed(v)
 
 func set_health(v):
 	health = v
+	emit_signal("health_changed", health)
 	if health <= 0:
 		emit_signal("death")
+
+func set_max_health(v):
+	max_health = v
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
+
+func _ready():
+	self.health = max_health
---
+++ UI/HealthUI.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var health = 4 setget set_health
+var max_health = 4 setget set_max_health
+
+onready var empty = $HeartUIEmpty
+onready var full = $HeartUIFull
+
+func set_health(v):
+	health = clamp(v, 0, max_health)
+	if full != null:
+		print(health)
+		full.rect_size.x = health * 15
+	
+func set_max_health(v):
+	max_health = max(v, 1)
+	if empty != null:
+		empty.rect_size.x = max_health * 15
+	
+func _ready():
+	self.max_health = PlayerStats.max_health
+	self.health = PlayerStats.health
+	PlayerStats.connect("health_changed", self, "set_health")
+	PlayerStats.connect("max_health_changed", self, "set_max_health")
